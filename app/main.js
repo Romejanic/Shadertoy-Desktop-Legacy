b779c460dce4eba2d3e83a96ad25057e8f8d9a97
Romejanic <romejanic@gmail.com>
2018-08-28 12:17:17 +1000

    Boilerplate Electron code

---
+++ app/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const windows = {
+    mainWindow: undefined,
+
+    createMainWindow: function() {
+        if(windows.mainWindow) {
+            return;
+        }
+        var win = new BrowserWindow({
+            title: "Shadertoy",
+            minWidth: 200,
+            minHeight: 200,
+            resizable: true
+        });
+        win.on("closed", () => {
+            delete windows.mainWindow;
+        });
+        win.loadURL("file://" + path.join(__dirname, "ui", "index.html"));
+        windows.mainWindow = win;
+    }
+};
+
+process.isMac = process.platform.indexOf("darwin") > -1;
+
+app.on("ready", windows.createMainWindow);
+app.on("activate", windows.createMainWindow);
+app.on("window-all-closed", () => {
+    if(!process.isMac) {
+        app.quit();
+    }
+});
